0a146fa161fa64fa4926fb8bfbfb31ee6bd4a2cf
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-10-11 22:33:22 +0200

    feat: export to csv

--- src/extract.py
+++ src/extract.py
@@ -6,7 +6,7 @@ from collections import namedtuple
 import boto3
 
 S3Query = namedtuple("S3Query", "bucket prefix")
-S3FileData = namedtuple("S3FileData", "name date size")
+S3FileData = dict
 S3Data = list[S3FileData]
 
 logger = logging.getLogger(__name__)
@@ -17,6 +17,7 @@ def run():
     print(f"Working with {s3_query}")
     s3_data = _get_s3_data(s3_query)
     _export_to_csv(s3_data)
+    print("Extraction done")
 
 
 def _get_s3_query_from_user_input() -> S3Query:
@@ -31,28 +32,28 @@ def _get_s3_query_from_user_input() -> S3Query:
     return S3Query(bucket_name, path_name)
 
 
-def _get_s3_data(s3_query: S3Query) -> S3FileData:
+def _get_s3_data(s3_query: S3Query) -> S3Data:
     session = boto3.Session()
     s3_client = session.client('s3')
     response = s3_client.list_objects_v2(Bucket=s3_query.bucket, Prefix=s3_query.prefix)
     if response["IsTruncated"] is True:
         raise ValueError("I can't manage all the files")
     return [
-        S3FileData(
-            obj["Key"].split("/")[-1],
-            obj["LastModified"],
-            obj["Size"],
-        )
-        for obj in response['Contents']
+            {
+                "name": content["Key"].split("/")[-1],
+                "date": content["LastModified"],
+                "size": content["Size"],
+            }
+        for content in response['Contents']
     ]
 
 
 def _export_to_csv(s3_data: S3Data):
-    with open('output.csv', 'w') as f:
-        w = csv.writer(f)
-        w.writerow(s3_data[0]._fields)    # field header
-        w.writerows([(value for value in getattr(file_data, field_name)) for file_data in s3_data for field_name in file_data._fields])
-
+    with open("output.csv", "w", newline="") as f:
+        w = csv.DictWriter(f, s3_data[0].keys())
+        w.writeheader()
+        for file_data in s3_data:
+            w.writerow(file_data)
 
 
 if __name__ == "__main__":
