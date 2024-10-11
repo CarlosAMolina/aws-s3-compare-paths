0bf8347567e87a0fd621a2491ed73adf2d219892
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-10-11 16:24:28 +0200

    feat: exception if not all files can be managed

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 def run():
     s3_query = _get_s3_query_from_user_input()
     print(f"Working with {s3_query}")
-    s3_data = get_s3_data(s3_query)
+    s3_data = _get_s3_data(s3_query)
     print(s3_data) # TODO rm
 
 
@@ -30,10 +30,12 @@ def _get_s3_query_from_user_input() -> S3Query:
     return S3Query(bucket_name, path_name)
 
 
-def get_s3_data(s3_query: S3Query) -> S3FileData:
+def _get_s3_data(s3_query: S3Query) -> S3FileData:
     session = boto3.Session()
     s3_client = session.client('s3')
     response = s3_client.list_objects_v2(Bucket=s3_query.bucket, Prefix=s3_query.prefix)
+    if response["IsTruncated"] is True:
+        raise ValueError("I can't manage all the files")
     return [
         S3FileData(
             obj["Key"].split("/")[-1],
