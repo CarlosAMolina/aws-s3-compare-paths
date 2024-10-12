92db1e0576b740535d787a430c3bd48ddc0bc567
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-10-12 10:02:37 +0200

    feat: add multiindex

--- src/compare.py
+++ src/compare.py
@@ -11,7 +11,7 @@ def run():
     file_path_names = _get_s3_file_name_paths_from_user_input()
     print(f"Start comparing {' and '.join(file_path_names)}")
     s3_data_df = _get_df_combine_files(file_path_names)
-    _show_summary(s3_data_df, file_path_names)
+    #_show_summary(s3_data_df, file_path_names)
     s3_analyzed_df = _get_df_analyze_s3_data(s3_data_df, file_path_names)
     print(s3_analyzed_df)
 
@@ -30,7 +30,20 @@ def _get_s3_file_name_paths_from_user_input() -> FilePathNamesToCompare:
 def _get_df_combine_files(file_path_names: FilePathNamesToCompare) -> Df:
     file_1_df = _get_df_from_file(file_path_names, 0)
     file_2_df = _get_df_from_file(file_path_names, 1)
-    return file_1_df.join(file_2_df, how='outer')
+    result = file_1_df.join(file_2_df, how='outer')
+    result.columns = pd.MultiIndex.from_tuples(_get_column_names_multindex(result))
+    return result
+
+def _get_column_names_multindex(column_names: list[str]) -> list[tuple[str, str]]:
+    return [
+        _get_tuple_column_names_multindex(column_name)
+        for column_name in column_names
+    ]
+
+def _get_tuple_column_names_multindex(column_name: str) -> tuple[str, str]:
+    column_name_clean = column_name.replace("_file","")
+    indexes = column_name_clean.split("_")
+    return indexes[1], indexes[0]
 
 
 def _get_df_from_file(file_path_names: FilePathNamesToCompare, file_index: int) -> Df:
@@ -42,17 +55,34 @@ def _get_df_from_file(file_path_names: FilePathNamesToCompare, file_index: int) 
 
 
 def _get_df_analyze_s3_data(df: Df, file_path_names: FilePathNamesToCompare) -> Df:
-    condition_exists = (df["size_file_0"].notnull()) & (df["size_file_1"].notnull())
-    df["exists_file_in_both_paths"] = condition_exists
-    df.loc[
-        (df["exists_file_in_both_paths"].eq(False)) & (df["size_file_0"].notnull()),
-        "unique_path_where_the_file_exists",
-    ] = file_path_names[0]
-    df.loc[
-        (df["exists_file_in_both_paths"].eq(False)) & (df["size_file_1"].notnull()),
-        "unique_path_where_the_file_exists",
-    ] = file_path_names[1]
-    df["has_file_same_size_in_both_paths"] = (df["exists_file_in_both_paths"].eq(True)) & (df["size_file_0"] == df["size_file_1"])
+    condition_exists = (
+        df.loc[:, ("0", "size")].notnull()
+    ) & (
+        df.loc[:, ("1", "size")].notnull()
+    )
+    # https://stackoverflow.com/questions/18470323/selecting-columns-from-pandas-multiindex
+    df[[("analysis","exists_file_in_both_paths"),]] = False
+    df.loc[condition_exists, [("analysis","exists_file_in_both_paths"),]] = True
+    condition = (
+        df.loc[:, ("analysis", "exists_file_in_both_paths")].eq(False)
+    ) & (
+        df.loc[:, ("0", "size")].notnull()
+    )
+    df.loc[condition, "unique_path_where_the_file_exists"] = file_path_names[0]
+    condition = (
+        df.loc[:, ("analysis", "exists_file_in_both_paths")].eq(False)
+    ) & (
+        df.loc[:, ("1", "size")].notnull()
+    )
+    df.loc[condition, "unique_path_where_the_file_exists"] = file_path_names[1]
+
+    condition = (
+        df.loc[:, ("analysis", "exists_file_in_both_paths")].eq(True)
+    ) & (
+        df.loc[:, ("0", "size")] == df.loc[:, ("0", "size")]
+    )
+    df.loc[condition, "has_file_same_size_in_both_paths"] = False
+    df.loc[condition, "has_file_same_size_in_both_paths"] = True
     return df
 
 
