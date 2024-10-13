3dbff8234fe3b66a83448eb88bf40687db8ea0c7
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-10-13 11:05:04 +0200

    feat(extract): get s3 queries from config variable instead of user input

--- src/extract.py
+++ src/extract.py
@@ -5,6 +5,15 @@ from collections import namedtuple
 
 import boto3
 
+
+config = {
+    "bucket": "pets",
+    "paths": [
+        "dogs/big/funds/",
+        "cats/europe/",
+    ]
+}
+
 S3Query = namedtuple("S3Query", "bucket prefix")
 S3FileData = dict
 S3Data = list[S3FileData]
@@ -13,50 +22,23 @@ logger = logging.getLogger(__name__)
 
 
 def run():
-    for s3_query in _get_s3_queries():
-        _run_s3_path_files_to_csv(s3_query)
+    print(_get_s3_queries())
+#    for s3_query in _get_s3_queries():
+#        _run_s3_path_files_to_csv(s3_query)
 
 
 def _get_s3_queries() -> list[S3Query]:
-    error_text = "The first argumen must be i or f.\nUsage: python extract.py [i|f]"
-    if len(sys.argv) > 1:
-        if sys.argv[1] == "i":
-            return [_get_s3_query_from_user_input()]
-        if sys.argv[1] == "f":
-            return _get_s3_queries_from_file()
-    raise ValueError(error_text)
-
+    bucket_name = config["bucket"]
+    path_names = config["paths"]
+    return [
+        S3Query(bucket_name, path_name)
+        for path_name in path_names
+    ]
 
 def _run_s3_path_files_to_csv(s3_query):
     print(f"Working with {s3_query}")
     s3_data = _get_s3_data(s3_query)
     _export_to_csv(s3_data, s3_query.prefix)
-
-
-def _get_s3_query_from_user_input() -> S3Query:
-    user_input = sys.argv
-    try:
-        bucket_name, path_name = user_input[2], user_input[3]
-    except IndexError:
-        raise ValueError(
-            "Usage: python extract.py i {bucket} {prefix}"
-            "\nExample: python extract.py i pets dogs/husky/"
-        )
-    return S3Query(bucket_name, path_name)
-
-def _get_s3_queries_from_file() -> list[S3Query]:
-    user_input = sys.argv
-    file_name_paths_to_analyze = "paths-to-analyze.txt"
-    try:
-        bucket_name = user_input[2]
-    except IndexError:
-        raise ValueError(
-            "Usage: python extract.py f {bucket_name}"
-            "\nExample: python extract.py f pets"
-        )
-    with open(file_name_paths_to_analyze, "r") as f:
-        file_path_names = f.read().splitlines()
-    return [S3Query(bucket_name, file_path_name) for file_path_name in file_path_names]
 
 
 def _get_s3_data(s3_query: S3Query) -> S3Data:
