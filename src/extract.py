2f01d0d2ec86e7ae200fbe1c9347399706b146fd
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-10-13 12:09:47 +0200

    feat(extract): improve export to file

--- src/extract.py
+++ src/extract.py
@@ -1,7 +1,9 @@
 import csv
 import logging
-import sys
+import os
+import os.path
 from collections import namedtuple
+from pathlib import PurePath
 
 import boto3
 
@@ -9,7 +11,7 @@ import boto3
 config = {
     "bucket": "pets",
     "paths": [
-        "dogs/big/funds/",
+        "dogs/big/",
         "cats/europe/",
     ]
 }
@@ -18,27 +20,25 @@ S3Query = namedtuple("S3Query", "bucket prefix")
 S3FileData = dict
 S3Data = list[S3FileData]
 
+
 logger = logging.getLogger(__name__)
 
 
 def run():
-    print(_get_s3_queries())
-#    for s3_query in _get_s3_queries():
-#        _run_s3_path_files_to_csv(s3_query)
+    s3_queries = _get_s3_queries()
+    for query_index, s3_query in enumerate(s3_queries, 1):
+        print(f"Working with query {query_index}/{len(s3_queries)}: {s3_query}")
+        s3_data = _get_s3_data(s3_query)
+        _export_to_csv(s3_data, s3_query)
 
 
 def _get_s3_queries() -> list[S3Query]:
-    bucket_name = config["bucket"]
+    bucket = config["bucket"]
     path_names = config["paths"]
     return [
-        S3Query(bucket_name, path_name)
+        S3Query(bucket, path_name)
         for path_name in path_names
     ]
-
-def _run_s3_path_files_to_csv(s3_query):
-    print(f"Working with {s3_query}")
-    s3_data = _get_s3_data(s3_query)
-    _export_to_csv(s3_data, s3_query.prefix)
 
 
 def _get_s3_data(s3_query: S3Query) -> S3Data:
@@ -61,17 +61,37 @@ def _get_file_name_from_response_key(content: dict) -> str:
     return content["Key"].split("/")[-1]
 
 
-def _export_to_csv(s3_data: S3Data, s3_path_name: str):
-    file_name = s3_path_name.replace('/','_')
-    file_name = file_name[:-1] if file_name.endswith('_') else file_name
+def _export_to_csv(s3_data: S3Data, s3_query: S3Query):
+    bucket_path= _get_folder_path_for_bucket_export(s3_query.bucket)
+    _create_folder_if_not_exists(bucket_path)
+    file_path = _get_file_path(bucket_path, s3_query.prefix)
+    _export_data_to_csv(s3_data, file_path)
+    print(f"Extraction done")
+
+def _create_folder_if_not_exists(path: PurePath):
+    if not os.path.isdir(path):
+        print("Creating folder", path)
+        os.makedirs(path)
+
+
+def _get_folder_path_for_bucket_export(bucket: str) -> PurePath:
+    return PurePath("exports", bucket)
+
+
+def _get_file_path(bucket_path: PurePath, s3_path_name: str) -> PurePath:
+    s3_path_name_clean = s3_path_name[:-1] if s3_path_name.endswith('/') else s3_path_name
+    file_name = s3_path_name_clean.replace('/','-')
     file_name = f"{file_name}.csv"
-    file_path_name = f"exports/{file_name}"
-    with open(file_path_name, "w", newline="") as f:
+    return bucket_path.joinpath(file_name)
+
+
+def _export_data_to_csv(s3_data: S3Data, file_path: PurePath):
+    print(f"Exporting data to {file_path}")
+    with open(file_path, "w", newline="") as f:
         w = csv.DictWriter(f, s3_data[0].keys())
         w.writeheader()
         for file_data in s3_data:
             w.writerow(file_data)
-    print(f"Extraction done: {file_path_name}")
 
 
 if __name__ == "__main__":
